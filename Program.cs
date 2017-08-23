8f17bdddeb0926469d0d969c395ddf22ade81e13
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-23 07:42:00 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lista21082017ex03
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            decimal salarioAtual, percentualReajuste, salarioReajustado;
+			Console.WriteLine("Digite o salário atual: ");
+            salarioAtual = Convert.ToDecimal(Console.ReadLine());
+			Console.WriteLine("Digite o porcentual de reajuste: ");
+            percentualReajuste = Convert.ToDecimal(Console.ReadLine());
+			percentualReajuste = (percentualReajuste / 100) + 1;
+			salarioReajustado = salarioAtual * percentualReajuste;
+            Console.Write("O valor do salário atualizado é {0}", salarioReajustado);
+			Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Lista21082017ex03 C# Curso Programação de Dispositivos Móveis SENAC
